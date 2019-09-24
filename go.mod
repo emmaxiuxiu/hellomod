8bc627abb734fbdfcb38176858f598c38c743877
Emma Xiu <emma.xiu@oracle.com>
2019-09-23 18:06:39 -0700

    Add deps

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/emmaxiuxiu/hellomod
 
 go 1.12
+
+require github.com/donvito/hellomod v1.0.1
--- src/main.go
+++ src/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/donvito/hellomod"
 	"github.com/emmaxiuxiu/hellomod/src/utils"
 )
 
 //SayHello function
 func main() {
 	fmt.Println(utils.AddHola("Hello World"))
+	hellomod.SayHello()
 }
